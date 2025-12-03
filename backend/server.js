eda95e7c2f3c254efe3865f8dfa2e2fe3273a0a4
Song Chi <111693600+Bnkcheesus@users.noreply.github.com>
2025-12-03 18:14:46 +0700

    add routes (fix)

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/authors", require("./routes/authors"));
 app.use("/api/books", require("./routes/books"));
+app.use("/api/carts", require("./routes/carts"));
+app.use("/api/orders", require("./routes/orders"));
+app.use("/api/reviews", require("./routes/reviews"));
+app.use("/api/tags", require("./routes/tags"));
+app.use("/api/users", require("./routes/users"));
 
 const PORT = 5000;
 app.listen(PORT, () => console.log("Server running on port " + PORT));
