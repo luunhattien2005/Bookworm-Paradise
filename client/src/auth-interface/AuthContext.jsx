10f695f2c24d5cea545598c704a76219d81773f5
Tien Luu <luunhattien2005@gmail.com>
2025-12-11 00:20:47 +0700

    quick fix logout logic

--- client/src/auth-interface/AuthContext.jsx
+++ client/src/auth-interface/AuthContext.jsx
@@ -5,7 +5,9 @@ export const  AuthContext = createContext()
 export function AuthProvider({children}) {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
-
+    const img_url = "https://dummyimage.com/50/000/fff"
+    const [logoutToHome, setLogoutToHome] = useState(false)
+    
     // Load từ localStorage khi mở website
     useEffect(() => {
         const savedUser = localStorage.getItem("user")
@@ -20,7 +22,7 @@ export function AuthProvider({children}) {
     const login = async (username, password) => {
         // gọi API login
         // const res = await fetch(…)
-        const fakeUser = { username }; // ví dụ thôi
+        const fakeUser = { username, img_url }; // ví dụ thôi
 
         setUser(fakeUser);
         localStorage.setItem("user", JSON.stringify(fakeUser));
@@ -29,7 +31,7 @@ export function AuthProvider({children}) {
 
     // Hàm đăng ký
     const signup = async (username, email, password) => {
-        const fakeUser = { username }; // ví dụ thôi
+        const fakeUser = { username, img_url }; // ví dụ thôi
 
         setUser(fakeUser);
         localStorage.setItem("user", JSON.stringify(fakeUser));
@@ -37,12 +39,15 @@ export function AuthProvider({children}) {
     }
 
     // Hàm đăng xuất
-    const logout = () => {
+    const logout = (callback) => {
         setUser(null);
+        setLogoutToHome(true);
         localStorage.removeItem("user");
+
+        setTimeout(() => setLogoutToHome(false), 10);
     };
 
-    const value = { user, loading, login, signup, logout };
+    const value = { user, loading, logoutToHome, login, signup, logout };
 
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
