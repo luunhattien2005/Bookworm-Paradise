d5d935413ad1e8cf14130ba318afcffbe8177041
Tien Luu <luunhattien2005@gmail.com>
2025-12-11 09:06:51 +0700

    Add inter font, fix files name, fix header shown in /auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,40 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import ProtectedRoute from "./auth-interface/ProtectedRoute.jsx";
 import Card from "./Card.jsx";
 import Auth from "./auth-interface/Auth.jsx";
 import Logout from "./auth-interface/Logout.jsx";
 import Welcome from "./auth-interface/Welcome.jsx";
 import Header from "./header-footer-interface/Header.jsx";
+import Dashboard from "./admin-interface/components/dashboard/Dashboard.jsx";
 
 export default function App() {
-
+  const location = useLocation()
 
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-            <Route path="/" element={<Welcome />} />
+      
+      {location.pathname !== "/auth" && <Header />}        
+      <Routes>
+          <Route path="/" element={<Welcome />} />
 
-            <Route path="/auth" element={<Auth />} />
+          <Route path="/ad" element={<Dashboard />} />
 
-            <Route path="/logout" element={<Logout />} />
-            
-            <Route path="/home" element={ 
-                <Card name="Luu Nhat Tien" school="HCMUS" ID={23127127}/>
-              } />
+          <Route path="/auth" element={<Auth />} />
 
-            <Route path="/profile" element={
-                <ProtectedRoute>
-                  <Card name="Luu Nhat Tien" school="HEHEE" ID={2323230}/>
-                </ProtectedRoute>
-              } />
-            
-        </Routes>
-      </BrowserRouter>
+          <Route path="/logout" element={<Logout />} />
+          
+          <Route path="/home" element={ 
+              <Card name="Luu Nhat Tien" school="HCMUS" ID={23127127}/>
+            } />
+
+          <Route path="/profile" element={
+              <ProtectedRoute>
+                <Card name="Luu Nhat Tien" school="HEHEE" ID={2323230}/>
+              </ProtectedRoute>
+            } />
+          
+      </Routes>
+      
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './auth-interface/AuthContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 // import Auth from './auth-interface/Auth.jsx'
@@ -8,8 +9,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
