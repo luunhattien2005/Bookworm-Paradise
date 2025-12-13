9afcdae74c427dc0a697f065d153a67ac4043d86
haimumuoi <minhquanzero@gmail.com>
2025-12-13 19:05:26 +0700

    fix review model

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const ReviewSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'Account', required: true },
     book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
-    
     rating: { type: Number, required: true, min: 1, max: 5 },
     comment: { type: String, maxlength: 500 }, 
-    
-}, { timestamps: true });
+    reviewDate: { type: Date, default: Date.now }
+});
 
 module.exports = mongoose.model('Review', ReviewSchema);
