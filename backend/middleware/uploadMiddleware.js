5618a0ee93427a1523290b638df00b9c4b2ab12a
haimumuoi <minhquanzero@gmail.com>
2025-12-13 19:38:31 +0700

    uploadmiddleware

---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,24 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); 
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+// Bộ lọc chỉ chấp nhận ảnh
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+        cb(null, true);
+    } else {
+        cb(new Error('Chỉ được upload file ảnh!'), false);
+    }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+module.exports = upload;
