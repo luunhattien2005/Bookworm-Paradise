899c325d4d54b5bc2ffc9c08356a0df0dfc1660d
haimumuoi <minhquanzero@gmail.com>
2025-12-13 18:44:10 +0700

    fix cart model

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const CartItemSchema = new mongoose.Schema({
     book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
     quantity: { type: Number, required: true, min: 1 },
+    subTotal: { type: Number, default: 0 }
 });
 
 const CartSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'Account', required: true, unique: true },
     items: [CartItemSchema],
-    // totalAmount cũng nên tính toán động (dynamic) thay vì lưu cứng
+    totalAmount: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('Cart', CartSchema);
