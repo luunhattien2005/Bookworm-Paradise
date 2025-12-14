f2e3122212394c47084d029f02502a0d6e1969ca
Tien Luu <luunhattien2005@gmail.com>
2025-12-14 15:44:37 +0700

    Add page name header + it css

---
+++ client/src/header-footer-interface/PageNameHeader.jsx
@@ -0,0 +1,20 @@
+import styles from "./HeaderFooter.module.css"
+
+export default function PageNameHeader({ pagename }) {
+
+    const icons = {
+        Home: ['Home page', <i className="material-symbols-outlined" style={{ fontSize: "36px"}}>home</i>],
+        Admin: ['Admin dashbroad',''],
+        Profile: ['Your profile', <i className="fa-regular fa-circle-user" style={{ fontSize: "30px"}}></i>],
+        Cart: ['Shopping cart',''],
+    }
+
+    return(
+        <div className={styles.pageNameGradient}>
+            <div className={styles.nameContainer}>
+                {icons[pagename][1]} {icons[pagename][0]}
+            </div>
+        </div>
+    )
+}
+
