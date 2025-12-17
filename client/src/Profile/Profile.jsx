d557983400e5a35a72da928b6f1ea102742977eb
nmdat23 <nmdat23@clc.fitus.edu.vn>
2025-12-17 17:07:12 +0700

    Upload Profile Page (React)

---
+++ client/src/Profile/Profile.jsx
@@ -0,0 +1,44 @@
+import { useContext } from 'react';
+import { AuthContext } from '../auth-interface/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import styles from "./Profile.module.css";
+
+export default function Profile() {
+    const { user } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    return (
+    <>
+        <div className={styles.headerGradient}> 
+            <i className="fa-regular fa-circle-user" style={{fontSize: "30px"}}></i>
+            <a>Your Profile</a> 
+	    </div>
+
+        <main className={styles.container}>
+            <div className={styles.leftContainer}>
+                <div className={styles.upperContainer}>
+                    <div className={styles.ProfilePicture}>
+                        <i className="material-symbols-outlined">photo_camera_front</i>
+                        <img src="/img/PP_Large.png" alt="Picture Profile here"></img>
+                    </div>
+
+                    <p>Lưu Nhật Tiến</p>
+                </div>
+
+                <div className={styles.lowerContainer}>
+                    <p>Hồ sơ cá nhân</p>
+                    <p>Thay đổi mật khẩu</p>
+                    <p>Sách yêu thích</p>
+                    <p>Lịch sử mua hàng</p>
+                    <p>Trung tâm thông báo</p>
+                    <p>This is useless button</p>
+                    <p style={{color: "rgb(255, 0, 0)"}} onClick={() => { navigate("/logout")}}>Đăng xuất</p>
+                </div>
+            </div>
+        </main>
+
+
+
+
+    </>)
+}
