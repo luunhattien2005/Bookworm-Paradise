30da5800667ca2daa1cf92978563975c61c77543
NathanBackward2005 <tdhthuong23@clc.fitus.edu.vn>
2025-12-23 21:53:08 +0700

    Update useWishlist.js
    v5 useWishlist.js

--- client/src/hooks/useWishlist.js
+++ client/src/hooks/useWishlist.js
@@ -1,43 +1,57 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
 import * as wishlistApi from '../api/wishlist';
 
 /**
- * useWishlistQuery(userId)
- * useAddToWishlist / useRemoveFromWishlist
+ * Get wishlist for a user
  */
-
-export function useWishlistQuery(userId, options = {}) {
-  return useQuery(['wishlist', userId], () => wishlistApi.getWishlist(userId), {
+export function useWishlist(userId, options = {}) {
+  return useQuery({
+    queryKey: ['wishlist', userId],
+    queryFn: () => wishlistApi.getWishlist(userId),
     enabled: !!userId,
-    staleTime: 1000 * 60,
+    staleTime: 1000 * 60, // 1 minute
     ...options,
   });
 }
 
+/**
+ * Add book to wishlist
+ */
 export function useAddToWishlist(userId, options = {}) {
   const qc = useQueryClient();
-  return useMutation(
-    (bookId) => wishlistApi.addToWishlist(userId, bookId),
-    {
-      onSuccess() {
-        qc.invalidateQueries(['wishlist', userId]);
-        if (options.onSuccess) options.onSuccess();
-      },
-      ...options,
-    }
-  );
+
+  return useMutation({
+    mutationFn: (bookId) =>
+      wishlistApi.addToWishlist(userId, bookId),
+
+    onSuccess: (...args) => {
+      qc.invalidateQueries({ queryKey: ['wishlist', userId] });
+      options.onSuccess?.(...args);
+    },
+
+    ...options,
+  });
 }
 
+/**
+ * Remove book from wishlist
+ */
 export function useRemoveFromWishlist(userId, options = {}) {
   const qc = useQueryClient();
-  return useMutation(
-    (bookId) => wishlistApi.removeFromWishlist(userId, bookId),
-    {
-      onSuccess() {
-        qc.invalidateQueries(['wishlist', userId]);
-        if (options.onSuccess) options.onSuccess();
-      },
-      ...options,
-    }
-  );
+
+  return useMutation({
+    mutationFn: (bookId) =>
+      wishlistApi.removeFromWishlist(userId, bookId),
+
+    onSuccess: (...args) => {
+      qc.invalidateQueries({ queryKey: ['wishlist', userId] });
+      options.onSuccess?.(...args);
+    },
+
+    ...options,
+  });
 }
