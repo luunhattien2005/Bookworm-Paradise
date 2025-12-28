d57c33faff6be5883394555cb216db74f85c4e8b
nmdat23 <nmdat23@clc.fitus.edu.vn>
2025-12-29 04:56:51 +0700

    Adding Stylying to Admin Orders

--- client/src/admin-interface/OrderEdit.jsx
+++ client/src/admin-interface/OrderEdit.jsx
@@ -3,6 +3,8 @@ import { useParams, useNavigate } from "react-router-dom"
 import { useState, useEffect } from "react"
 import { useOrderById, useUpdateOrderStatus } from "../hooks/useOrder"
 import Loading from "../header-footer-interface/Loading"
+import PageNameHeader from "../header-footer-interface/PageNameHeader"
+import { useDialog } from "../header-footer-interface/DialogContext";
 
 export default function OrderEdit() {
     const { id } = useParams()
@@ -11,13 +13,16 @@ export default function OrderEdit() {
 
     const { data: order, isLoading } = useOrderById(id);
 
+    // Dialog set up
+    const { showDialog } = useDialog();
+
     const updateStatusMutation = useUpdateOrderStatus({
         onSuccess: () => {
-            alert("Cập nhật trạng thái thành công!");
+            showDialog("Cập nhật trạng thái thành công!");
             navigate("/admin/dashboard");
         },
         onError: (err) => {
-            alert(err.response?.data?.message || "Lỗi cập nhật");
+            showDialog(err.response?.data?.message || "Lỗi cập nhật");
         }
     });
 
@@ -36,75 +41,126 @@ export default function OrderEdit() {
     const isLocked = order.status === "Delivered" || order.status === "Cancelled";
 
     return (
-        <div className={styles.content}>
-            <h2>Chi tiết đơn hàng: {id}</h2>
-            <div className={styles.orderEditContainer}>
-                <div className={styles.orderDetails}>
-                    <p><strong>Khách hàng:</strong> {order.user?.fullname}</p>
-                    <p><strong>Địa chỉ:</strong> {order.shippingAddress}</p>
-                    <p><strong>Tổng tiền:</strong> {Number(order.totalAmount).toLocaleString()} đ</p>
-                </div>
+        <>
+            <PageNameHeader pagename="Admin"/>
 
-                <div className={styles.statusSection}>
-                    <h3>Cập nhật trạng thái:</h3>
-                    
-                    {isLocked && <p style={{color: "red", fontStyle: "italic"}}>* Đơn hàng đã giao thành công, không thể thay đổi trạng thái.</p>}
+            <div className={styles.content}>
+                <div className={styles.OrderTitle}>
+                    <h1> Cập nhật thông tin đơn hàng</h1>
 
-                    <div className={styles.radioGroup}>
-                        
-                        <label>
-                            <input 
-                                type="radio" 
-                                checked={status === "Pending"} 
-                                onChange={() => setStatus("Pending")} 
-                                disabled={isLocked} // Khóa nếu đã Delivered
-                            /> 
-                            Pending (Chờ xử lý)
-                        </label>
+                    <div className={styles.OrderTitleRight}>
+                        {!isLocked && (
+                            <button className={styles.updateButton} onClick={handleUpdate}>
+                                LƯU TRẠNG THÁI
+                            </button>
+                        )}
 
-                        <label>
-                            <input 
-                                type="radio" 
-                                checked={status === "Processing"} 
-                                onChange={() => setStatus("Processing")} 
-                                disabled={isLocked}
-                            /> 
-                            Processing (Đang đóng gói)
-                        </label>
-
-                        <label>
-                            <input 
-                                type="radio" 
-                                checked={status === "Shipping"} 
-                                onChange={() => setStatus("Shipping")} 
-                                disabled={isLocked}
-                            /> 
-                            Shipping (Đang giao)
-                        </label>
-
-                        <label>
-                            <input 
-                                type="radio" 
-                                checked={status === "Delivered"} 
-                                onChange={() => setStatus("Delivered")} 
-                                disabled={isLocked}
-                            /> 
-                            Delivered (Đã giao)
-                        </label>
-
+                        <button className={styles.backButton} onClick={() => navigate(-1)} style={{ marginLeft: "10px" }}>
+                            QUAY LẠI
+                        </button>
                     </div>
+
                 </div>
 
-                {!isLocked && (
-                    <button className={styles.updateButton} onClick={handleUpdate}>
-                        LƯU TRẠNG THÁI
-                    </button>
-                )}
-                
-                <button className={styles.backButton} onClick={() => navigate(-1)} style={{ marginLeft: "10px" }}>
-                    QUAY LẠI
-                </button>
+
+
+                <div className={styles.OrderEditBottom}>
+                    
+                    <div className={styles.OrderInfoEdit}>
+                        <div style={{display: "flex"}}>
+                            <h> Chi tiết đơn hàng: </h>
+                            <p> {id} </p>
+                        </div>
+
+                        <div style={{display: "flex"}}>
+                            <h1> Khách hàng: </h1>
+                            <p1> {order.user?.fullname} </p1>
+                        </div>
+                    </div>
+
+
+                    <div className={styles.OrderInfoEdit}>
+                        <div style={{display: "flex"}}>
+                            <h> Ngày giao: </h>
+                            <p> {new Date(order.createdAt).toLocaleDateString("vi-VN")} </p>
+                        </div>
+
+                        <div style={{display: "flex"}}>
+                            <h1> Tổng số tiền: </h1>
+                            <p1> {Number(order.totalAmount).toLocaleString()} VND </p1>
+                        </div>
+                    </div>
+                    
+                    <div className={styles.OrderInfoEdit}>
+                        <div style={{display: "flex"}}>
+                            <h> Địa chỉ: </h>
+                            <p> {order.shippingAddress} </p>
+                        </div>
+                    </div>
+
+                    <div className={styles.OrderInfoEdit}>
+                        <div style={{display: "flex"}}>
+                            <h> Phương thức: </h>
+                            <p> {order.paymentMethod} </p>
+                        </div>
+
+                        <div style={{display: "flex"}}>
+                            <h1> Trạng thái: </h1>
+                            <p1> {order.status} </p1>
+                        </div>
+                    </div>
+
+
+                    <div className={styles.statusSection}>
+                        <h3>Cập nhật trạng thái:</h3>
+                        
+                        {isLocked && <p style={{color: "red", fontStyle: "italic"}}>* Đơn hàng đã giao thành công, không thể thay đổi trạng thái.</p>}
+
+                        <div className={styles.radioGroup}>
+                            <label>
+                                <input 
+                                    type="radio" 
+                                    checked={status === "Pending"} 
+                                    onChange={() => setStatus("Pending")} 
+                                    disabled={isLocked} // Khóa nếu đã Delivered
+                                /> 
+                                Pending (Chờ xử lý)
+                            </label>
+
+                            <label>
+                                <input 
+                                    type="radio" 
+                                    checked={status === "Processing"} 
+                                    onChange={() => setStatus("Processing")} 
+                                    disabled={isLocked}
+                                /> 
+                                Processing (Đang đóng gói)
+                            </label>
+
+                            <label>
+                                <input 
+                                    type="radio" 
+                                    checked={status === "Shipping"} 
+                                    onChange={() => setStatus("Shipping")} 
+                                    disabled={isLocked}
+                                /> 
+                                Shipping (Đang giao)
+                            </label>
+
+                            <label>
+                                <input 
+                                    type="radio" 
+                                    checked={status === "Delivered"} 
+                                    onChange={() => setStatus("Delivered")} 
+                                    disabled={isLocked}
+                                /> 
+                                Delivered (Đã giao)
+                            </label>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
-        </div>
+        </>
     )
 }
